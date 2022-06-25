f6ed16b2690412b18910323d0da8aa137a537411
QueenFathi <105749763+QueenFathi@users.noreply.github.com>
2022-06-25 12:48:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const name = "Odutayo Fathiat";
+const height = "1.6m";
+const country = "Nigeria";
+
+document.write("My name is "+name+"\n");
+document.write("My height is "+height+"\n");
+document.write("I am from"+country)
